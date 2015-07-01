99ee7c4cc9e269802c3f6664ce4bf74f02fcd9e1
10Dozen <steel.mjr@gmail.com>
2015-07-02 00:30:37 +0300

    Update dialogs.hpp

--- PMC.Irina.MarketSystem2/dialogs.hpp
+++ PMC.Irina.MarketSystem2/dialogs.hpp
@@ -2,7 +2,7 @@ class dzn_market_dialog
 {
 	idd=-1;
 	movingenable=true;
-	
+	onLoad = "_this spawn dzn_fnc_market_setPurchaseDialogText"; 
 	class controls 
 	{		
 		////////////////////////////////////////////////////////
@@ -16,18 +16,23 @@ class dzn_market_dialog
 			y = 0.318 * safezoneH + safezoneY;
 			w = 0.364583 * safezoneW;
 			h = 0.168 * safezoneH;
-			colorBackground[] = {0,0,1,1};
-			colorActive[] = {0,0,1,1};
+			colorBackground[] = {0,0,0,0};
+			colorActive[] = {0,0,0,0};
+			style = 1;
 		};
 		class dzn_market_textbox: RscText
 		{
+			
 			idc = 1100;
-			text = "Purchase 17 items for total cost $3440"; //--- ToDo: Localize;
+			text = ""; //--- ToDo: Localize;
 			x = 0.325 * safezoneW + safezoneX;
 			y = 0.346 * safezoneH + safezoneY;
 			w = 0.335417 * safezoneW;
 			h = 0.084 * safezoneH;
-			colorBackground[] = {0,0,0,1};
+			
+			sizeEx = 0.04;
+			style = 530;
+			colorBackground[] = {0,0,0,0.8};
 		};
 		class dzn_market_button_yes: RscButton
 		{
@@ -37,6 +42,8 @@ class dzn_market_dialog
 			y = 0.43 * safezoneH + safezoneY;
 			w = 0.0729167 * safezoneW;
 			h = 0.028 * safezoneH;
+			colorBackground[] = {0,0,0,0.7};
+			action = "player call dzn_fnc_market_buttonYes";
 		};
 		class dzn_market_button_no: RscButton
 		{
@@ -46,6 +53,8 @@ class dzn_market_dialog
 			y = 0.43 * safezoneH + safezoneY;
 			w = 0.0729167 * safezoneW;
 			h = 0.028 * safezoneH;
+			colorBackground[] = {0,0,0,0.7};
+			action = "player call dzn_fnc_market_buttonNo";
 		};
 		////////////////////////////////////////////////////////
 		// GUI EDITOR OUTPUT END
